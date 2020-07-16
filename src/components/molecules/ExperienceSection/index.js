2b0e6312150b6282e8df3b1119233d3817ce8103
Jorge Junior <jorge.junior.568@live.com>
2020-07-16 14:14:02 -0300

    feat: experiences links

--- src/components/molecules/ExperienceSection/index.js
+++ src/components/molecules/ExperienceSection/index.js
@@ -1,37 +1,46 @@
-import React from "react"
-import useExperiences from "../../../hooks/experiences"
+import React from 'react'
+import useExperiences from '../../../hooks/experiences'
 
-function ExperienceSection () {
-  const experiences = useExperiences();
+function ExperienceSection() {
+  const experiences = useExperiences()
 
   return (
     <section id="experience">
       <h2>experience.</h2>
 
       <ul>
-        {
-          experiences
-            .map(experience => (
-              <li key={experience.id}>
-                <h3>{experience.title.toLowerCase()}</h3>
-                <h4 className={'dark'}>{experience.company_name.toLowerCase()}</h4>
-                <h4>{experience.started_at} - {experience.finished_at ?? 'currently'}</h4>
+        {experiences.map(experience => (
+          <li key={experience.id}>
+            <h3>{experience.title.toLowerCase()}</h3>
+            <a
+              href={experience.company_url}
+              target="_blank"
+              rel="noreferrer"
+              className={'dark'}
+            >
+              {experience.company_name.toLowerCase()}
+            </a>
+            <h4>
+              {experience.started_at} - {experience.finished_at ?? 'currently'}
+            </h4>
 
-                <p>
-                  {experience.description}
-                </p>
+            <p>{experience.description}</p>
 
-                <ul>
-                  <li>languages: <strong>{experience.languages.join(', ')}</strong></li>
-                  {
-                    experience.frameworks.length
-                      ? (<li>frameworks: <strong>{experience.frameworks.join(', ')}</strong></li>)
-                      : ''
-                  }
-                </ul>
+            <ul>
+              <li>
+                languages: <strong>{experience.languages.join(', ')}</strong>
               </li>
-            ))
-        }
+              {experience.frameworks.length ? (
+                <li>
+                  frameworks:{' '}
+                  <strong>{experience.frameworks.join(', ')}</strong>
+                </li>
+              ) : (
+                ''
+              )}
+            </ul>
+          </li>
+        ))}
       </ul>
     </section>
   )
