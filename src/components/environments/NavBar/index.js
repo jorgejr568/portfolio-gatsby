ef4f0dc00ded20fe897ebe301097edb033414ead
Jorge Junior <jorge.junior.568@live.com>
2020-07-16 21:11:26 -0300

    404 page

--- src/components/environments/NavBar/index.js
+++ src/components/environments/NavBar/index.js
@@ -1,27 +1,28 @@
-import React from "react";
-import { string } from "prop-types"
+import React from 'react'
+import { string } from 'prop-types'
+import { Link } from 'gatsby'
 
-export default function NavBar({siteTitle}) {
-    return (
-        <header className="header">
+export default function NavBar({ siteTitle }) {
+  return (
+    <header className="header">
+      <Link className={'brand'} to="/" title="Home Page Link">
+        <h1>{siteTitle}</h1>
+      </Link>
 
-            <a
-                className={'brand'}
-                href="/"
-                title="Home Page Link">
-                <h1>{siteTitle}</h1>
-            </a>
-
-            <button
-                className="button button-solid"
-                type={'button'}
-                onClick={e => {e.preventDefault(); alert('We\'re still building this part')}}>
-                portfolio
-            </button>
-        </header>
-    )
+      <button
+        className="button button-solid"
+        type={'button'}
+        onClick={e => {
+          e.preventDefault()
+          alert("We're still building this part")
+        }}
+      >
+        portfolio
+      </button>
+    </header>
+  )
 }
 
 NavBar.propTypes = {
-  siteTitle: string.isRequired
+  siteTitle: string.isRequired,
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/environments/Layout"
-import SEO from "../components/atoms/SEO"
+import Layout from '../components/environments/Layout'
+import SEO from '../components/atoms/SEO'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1 style={{ fontSize: '2rem', marginBottom: '1.2rem' }}>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
