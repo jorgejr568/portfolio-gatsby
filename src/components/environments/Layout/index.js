9efaffbf71566604ff05e0e76952aacdc505de70
Jorge Junior <jorge.junior.568@live.com>
2020-07-17 13:52:15 -0300

    feat: cache-control

--- src/components/environments/Layout/index.js
+++ src/components/environments/Layout/index.js
@@ -5,27 +5,29 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Footer from "../Footer"
-import NavBar from "../NavBar"
-import "../../../assets/styles/app.scss"
+import Footer from '../Footer'
+import NavBar from '../NavBar'
+import '../../../assets/styles/app.scss'
+import CacheMeta from '../../atoms/CacheMeta'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-      query SiteTitleQuery {
-          site {
-              siteMetadata {
-                  title
-              }
-          }
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
       }
+    }
   `)
 
   return (
     <>
+      <CacheMeta />
       <NavBar siteTitle={data.site.siteMetadata.title} />
       {children}
       <Footer />
